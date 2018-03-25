a3a60cdde9588c10cfff43a8131873fc813705b4
sallzzbr <evio.salgado@hotmail.com>
2018-03-25 11:12:11 -0300

    adjusting

--- AntonioSalgado_MaestroHero.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page1.playgroundpage/Contents.swift
+++ AntonioSalgado_MaestroHero.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page1.playgroundpage/Contents.swift
@@ -5,34 +5,13 @@
 //#-end-hidden-code
 /*:
  Welcome to my musical Playground,
- I always loved music, but I am really bad playing any instrument or singing.
- Thanks to technology I can play some music, I hope you enjoy it as I do.
- 
- **PLEASE RUN THIS BOOK ON A LANDSCAPE POSITION**
- 
- Just click on any musical note to start your own orchestra!
- 
- **The left hand** will slide up and down, with it you can control the volume of the last selected instrument.
- Move it up to increase the volume and down to decrease it.
- 
- **The right hand** will slide sideways, with it you can control the music rate.
- Move it to the left to increase the rate, to the right to decrease.
- 
- **The notes are**
- 
- A = La /
- B = Si /
- C = Do /
- D = Re /
- E = Mi /
- F = Fa /
- G = Sol /
- 
- Ps: my favourite song is Brothers in Arms - Dire Straits, maybe you can play it for me. 
- 
- Have fun, Maestro!
- 
+
  */
+//#-editable-code
+
+
+
+//#end-editable-code
 //#-hidden-code
 import PlaygroundSupport
 
