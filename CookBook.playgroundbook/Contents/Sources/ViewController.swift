629edb10f3a7828ddc6112bee4f694be9eeef849
sallzzbr <evio.salgado@hotmail.com>
2018-03-31 15:52:17 -0300

    recipes and images all ready and set

--- CookBook.playgroundbook/Contents/Sources/ViewController.swift
+++ CookBook.playgroundbook/Contents/Sources/ViewController.swift
@@ -41,7 +41,7 @@ public class ViewController: UIViewController {
         label.frame = CGRect(x: screenX*0.06, y: screenY*0.55, width: 380, height: 150)
         label.font = UIFont(name: "SinhalaSangamMN", size: 24)
         label.text = howTo
-        label.textColor = .black
+        label.textColor = .white
         label.alpha = 1
         label.lineBreakMode = NSLineBreakMode.byWordWrapping
         label.numberOfLines = 0
@@ -49,6 +49,12 @@ public class ViewController: UIViewController {
         //fried egg image
         let friedEggPng = "friedEgg.png"
         let friedEggImage = UIImage(named: friedEggPng)
+        
+        let scrambleEggPng = "scrambledEggs.png"
+        let scrambleEggImage = UIImage(named: scrambleEggPng)
+        
+        let omelettePng = "omelette.png"
+        let omeletteImage = UIImage(named: omelettePng)
         
         let lemonMoussePiePng = "lemonmoussepie.png"
         let lemonMoussePieImage = UIImage(named: lemonMoussePiePng)
@@ -61,6 +67,15 @@ public class ViewController: UIViewController {
         
         let carrotCakePng = "carrotCake.png"
         let carrotCakeImage = UIImage(named: carrotCakePng)
+        
+        let applePiePng = "applePie.png"
+        let applePieImage = UIImage(named: applePiePng)
+        
+        let breadPng = "bread.png"
+        let breadImage = UIImage(named: breadPng)
+        
+        let unkownFoodPng = "unkownfood.png"
+        let unkownFoodImage = UIImage(named: unkownFoodPng)
         
         func foodPictures(currentImage: UIImage, posX: CGFloat, posY: CGFloat, imageWidth: CGFloat, imageHeight: CGFloat){
             let foodImageView = UIImageView(image: currentImage)
@@ -76,58 +91,58 @@ public class ViewController: UIViewController {
         self.view = view
         
         func cooking(){
-            if ingredients.containsSameElements(as: friedEgg) || ingredients.containsSameElements(as: butterFriedEgg) && howTo == "pan"{
-                view.backgroundColor = .yellow
-                label.textColor = .black
+            if (ingredients.containsSameElements(as: friedEgg) || ingredients.containsSameElements(as: butterFriedEgg)) && howTo == "pan"{
+                view.backgroundColor = .blue
                 label.text = "Congratulations, you've made a fried egg! Let's try more complex recipes now!"
                 //IMAGE
                 foodPictures(currentImage: friedEggImage!, posX: screenX*0.15, posY: screenY*0.3, imageWidth: 224, imageHeight: 200)
             } else if ingredients.containsSameElements(as: lemonMousse) && howTo == "mix" {
-                label.text = "Lemon Mousse"
-                view.backgroundColor = .red
+                label.text = "Congratulations, you've made a Lemon Mousse! You are starting to learn!"
+                view.backgroundColor = .magenta
                 //IMAGE
-                foodPictures(currentImage: lemonMousseImage!, posX: screenX*0.15, posY: screenY*0.3, imageWidth: 258, imageHeight: 250)
+                foodPictures(currentImage: lemonMousseImage!, posX: screenX*0.13, posY: screenY*0.25, imageWidth: 258, imageHeight: 250)
             } else if ingredients.containsSameElements(as: applePie) && howTo == "bake" {
-                label.text = "Apple pie"
-                view.backgroundColor = .blue
+                label.text = "Congratulations, you've made an Apple pie! Was it hard for you?"
+                view.backgroundColor = .cyan
+                label.textColor = .black
+                //IMAGE
+                foodPictures(currentImage: applePieImage!, posX: screenX*0.12, posY: screenY*0.35, imageWidth: 250, imageHeight: 135)
             } else if ingredients.containsSameElements(as: brazilianCarrotCake) && howTo == "bake" {
-                view.backgroundColor = .black
-                label.textColor = .white
-                label.text = "Best carrot cake in the world!"
+                view.backgroundColor = .purple
+                label.text = "Congratulations, you've made the Best carrot cake in the world!"
                 //IMAGE
-                foodPictures(currentImage: carrotCakeImage!, posX: screenX*0.15, posY: screenY*0.3, imageWidth: 250, imageHeight: 166)
-            } else if ingredients.containsSameElements(as: cheeseCake) || ingredients.containsSameElements(as: lemonCheeseCake) || ingredients.containsSameElements(as: appleCheeseCake) && howTo == "bake" {
-                view.backgroundColor = .white
-                label.textColor = .black
-                label.text = "Cheese cake"
+                foodPictures(currentImage: carrotCakeImage!, posX: screenX*0.13, posY: screenY*0.33, imageWidth: 250, imageHeight: 166)
+            } else if (ingredients.containsSameElements(as: cheeseCake) || ingredients.containsSameElements(as: lemonCheeseCake) || ingredients.containsSameElements(as: appleCheeseCake)) && howTo == "bake" {
+                view.backgroundColor = .magenta
+                label.text = "Congratulations, you've made a cheesecake! There are different ways to do it and a lot of possible flavours!"
                 //IMAGE
-                foodPictures(currentImage: cheesecakeImage!, posX: screenX*0.15, posY: screenY*0.3, imageWidth: 252, imageHeight: 250)
+                foodPictures(currentImage: cheesecakeImage!, posX: screenX*0.13, posY: screenY*0.25, imageWidth: 250, imageHeight: 243)
             } else if ingredients.containsSameElements(as: lemonMoussePie) && howTo == "bake" {
-                view.backgroundColor = .white
-                label.textColor = .black
-                label.text = "Lemon mousse pie"
+                view.backgroundColor = .magenta
+                label.text = "Congratulations, you've made a Lemon mousse pie! That is was a challenge!"
                 //IMAGE
-                foodPictures(currentImage: lemonMoussePieImage!, posX: screenX*0.15, posY: screenY*0.3, imageWidth: 258, imageHeight: 250)
-            } else if ingredients.containsSameElements(as: friedEgg) || ingredients.containsSameElements(as: butterFriedEgg) && howTo == "mix"{
-                view.backgroundColor = .yellow
-                label.textColor = .black
+                foodPictures(currentImage: lemonMoussePieImage!, posX: screenX*0.13, posY: screenY*0.25, imageWidth: 258, imageHeight: 250)
+            } else if (ingredients.containsSameElements(as: friedEgg) || ingredients.containsSameElements(as: butterFriedEgg)) && howTo == "mix"{
+                view.backgroundColor = .blue
                 label.text = "Congratulations, you've made scrambled eggs! Let's try more complex recipes now!"
                 //IMAGE
-                foodPictures(currentImage: friedEggImage!, posX: screenX*0.15, posY: screenY*0.3, imageWidth: 224, imageHeight: 200)
-            } else if ingredients.containsSameElements(as: omelete) || ingredients.containsSameElements(as: butterOmelete) && howTo == "mix"{
-                view.backgroundColor = .yellow
-                label.textColor = .black
+                foodPictures(currentImage: scrambleEggImage!, posX: screenX*0.15, posY: screenY*0.3, imageWidth: 224, imageHeight: 200)
+            } else if (ingredients.containsSameElements(as: omelete) || ingredients.containsSameElements(as: butterOmelete)) && howTo == "mix"{
+                view.backgroundColor = .blue
                 label.text = "Congratulations, you've made an omelette! Let's try more complex recipes now!"
                 //IMAGE
-                foodPictures(currentImage: friedEggImage!, posX: screenX*0.15, posY: screenY*0.3, imageWidth: 224, imageHeight: 200)
+                foodPictures(currentImage: omeletteImage!, posX: screenX*0.15, posY: screenY*0.3, imageWidth: 250, imageHeight: 201)
             } else if ingredients.containsSameElements(as: bread) && howTo == "bake" {
-                view.backgroundColor = .white
+                view.backgroundColor = .cyan
                 label.textColor = .black
-                label.text = "Bread"
+                label.text = "Congratulations, you've made Bread! You are amazing!"
+                //IMAGE
+                foodPictures(currentImage: breadImage!, posX: screenX*0.15, posY: screenY*0.3, imageWidth: 224, imageHeight: 200)
             } else  {
-                view.backgroundColor = .white
-                label.textColor = .black
+                view.backgroundColor = .blue
                 label.text = "Look like you made something entirely new! Is it good or is it bad? Well we have to know it for sure."
+                //IMAGE
+                foodPictures(currentImage: unkownFoodImage!, posX: screenX*0.15, posY: screenY*0.3, imageWidth: 224, imageHeight: 200)
             }
             
         }
