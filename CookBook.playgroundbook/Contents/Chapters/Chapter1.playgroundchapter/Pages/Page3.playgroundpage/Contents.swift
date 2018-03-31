d61804303607e98bd470688bd890f3c6ab5c052a
sallzzbr <evio.salgado@hotmail.com>
2018-03-31 18:22:21 -0300

    v99 ready

---
+++ CookBook.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page3.playgroundpage/Contents.swift
@@ -0,0 +1,67 @@
+//#-hidden-code
+/*
+Copyright Antônio Salgado 2018. All Rights Reserved.
+*/
+//#-end-hidden-code
+
+/*:
+If you need some help with the recipes, here are the ones I've registered. This are my favorites!
+
+ ## Lemon Mousse:
+ lemon, sugar, salt, cream, egg
+- **Method:** mix
+
+ ## Lemon Mousse Pie:
+ lemon, sugar, salt, cream, egg, crackers
+- **Method:** bake
+
+## Apple pie:
+ flour, apple, sugar, butter, salt
+- **Method:** bake
+
+## Brazilian Carrot Cake:
+ carrot, eggs, sugar, flour, bakingPowder, salt, butter
+- **Method:** bake
+
+## Cheese Cake:
+ crackers, vanilla, butter, creamCheese, sugar, eggs
+- **Lemon Cheese Cake:**
+ crackers, vanilla, butter, creamCheese, sugar, eggs, lemon
+ - **Apple Cheese Cake:**
+ crackers, vanilla, butter, creamCheese, sugar, eggs, apple
+- **Method:** bake
+
+## Fried egg:
+ oil, egg
+- **Butter Fried egg:**
+ butter, egg
+- **Method:** pan
+
+## Scrambled egg:
+ oil, egg
+- **Butter scrambled egg:**
+ butter, egg
+- **Method:** mix
+
+ ## Omelette:
+ oil, egg, milk
+- **Butter Omelette:**
+ butter, egg, milk
+- **Method:** mix
+
+## Bread:
+ milk, flour, butter, sugar, salt
+- **Method:** bake
+
+ Notice that this recipes can be done in real life, but need more steps that could be more complicated.
+ Still, cooking isn't hard, like programming it demands training and dedication, have fun and tell me if you try some of this recipes someday!
+ Regards,
+*/
+
+
+
+
+
+
+
+
